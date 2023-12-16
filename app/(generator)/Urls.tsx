53dc7ad804fce679a7f7eef7b29a90c4c672d5d9
the6p4c <me@doggirl.gay>
2023-12-16 17:46:53 +1000

    copy embed url on click

--- app/(generator)/Urls.tsx
+++ app/(generator)/Urls.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 import styles from "./Urls.module.css";
 
@@ -15,6 +15,14 @@ export default function Urls({
   placeholderPost: string;
   onChange?: (post: string) => void;
 }) {
+  const [copyVisible, setCopyVisible] = useState(false);
+
+  const copyEmbed = async () => {
+    await navigator.clipboard.writeText(embed);
+    setCopyVisible(true);
+    setTimeout(() => setCopyVisible(false), 500);
+  };
+
   return (
     <div className={styles.urls}>
       <label>
@@ -27,6 +35,7 @@ export default function Urls({
           onInput={(e: ChangeEvent<HTMLInputElement>) =>
             onChange && onChange(e.target.value)
           }
+          title="enter post url"
           className={styles.post}
         />
       </label>
@@ -36,11 +45,37 @@ export default function Urls({
           {error}
         </div>
       ) : (
-        <div className={`${styles.embed} ${post ? "" : styles.placeholder}`}>
-          <span className={styles.visuallyHidden}>embed url: </span>
-          {embed}
-        </div>
+        <>
+          <div
+            onClick={copyEmbed}
+            title="click to copy"
+            className={`${styles.embed} ${post ? "" : styles.placeholder}`}
+          >
+            <span className={styles.visuallyHidden}>embed url: </span>
+            {embed}
+          </div>
+        </>
       )}
+      <div className={`${styles.copyOverlay} ${copyVisible && styles.visible}`}>
+        <CopyIcon />
+      </div>
     </div>
+  );
+}
+
+function CopyIcon() {
+  // heroicons document-duplicate
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className={styles.icon}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+      />
+    </svg>
   );
 }
